13df5ba8bdb8450751a47cffbd897f65d216b115
youssi <zzz123zz12@naver.com>
2025-05-30 11:02:30 +0900

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/themeProvider";
 import ClientLayout from "@/components/ClientLayout";
 import FabButton from '@/components/ui/FabButton';
-
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="ko"
      suppressHydrationWarning>
+        <Head>
+          <meta name="naver-site-verification" content="c86800ab7df395e4ca9853b414f8c5c1404ac8e9" />
+          <meta name="google-site-verification" content="6dJD4pGhO6YPIICZcyWGjhw54bczFf5ZRIQgip8wzYA" />
+       </Head>
       <body className={inter.className}>
       <ClientLayout>
         <ThemeProvider
